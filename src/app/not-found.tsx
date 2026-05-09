c47541ac3370c894c852bcdd89676ef2a1190ebb
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2026-05-09 16:41:17 +0100

    feat: Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <>
+      <h2 className="font-medium">Not Found</h2>
+      <p>This resource no longer exists or has been moved.</p>
+    </>
+  );
+};
+
+export default NotFound;
